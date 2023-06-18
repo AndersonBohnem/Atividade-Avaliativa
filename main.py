919742a069bdd0e332932468c6f5f6684d8c2789
AndersonBohnem <134561953+AndersonBohnem@users.noreply.github.com>
2023-06-18 19:22:48 -0300

    Adicionando uma biblioteca e funcionalidades.
    Nessa parte implementada adicionei uma biblioteca chamada tkinter e importei o simpledialog, de mode que eu consiga clicar na tela do programa e me retornar uma caixa de pergunta e printando uma resposta adequada pelo que foi digitado, implementando também a posição na qual foi clicada.

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import pygame
+#Aqui vou usar uma biblioteca chamada tkinter.
+from tkinter import simpledialog
 pygame.init()
 #Aqui temos o tamanho do nosso Space Marker.
 tamanho = (800,600)
@@ -13,13 +15,37 @@ tela = pygame.display.set_mode( tamanho )
 #Aqui temos a imagem utilizada como icone do programa.
 pygame.display.set_icon(pygame.image.load("space.png"))
 running = True 
+#Cor branca.
+branco = (255,255,255)
+itens = []
+
+#Isso é o raio do circulo criado quando adicioando uma estrela.
+raio = 3
+circulos = []
 
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        elif event.type == pygame.MOUSEBUTTONUP:
+            pos = pygame.mouse.get_pos()
+            item = simpledialog.askstring("Space" , "Nome da Estrela:")
+            print(item)
+            if item == "":
+                item = "desconhecido"+str(pos)
+            else:
+                item = item + str(pos)
+            itens.append((item, pos))
+            
+            
+
 
     tela.blit(fundo,(0,0))
+    for item , pos in itens:
+        mensagemDisplay = pygame.font.SysFont(None, 24).render (item , True , branco)
+        tela.blit(mensagemDisplay, pos)
+        pygame.draw.circle(tela , branco , pos , raio)
+
 
     pygame.display.update()
 
